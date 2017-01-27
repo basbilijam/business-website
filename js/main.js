96aa59965df07f8d63bb1cef93e3d72a1b250a50
kaan bursa <kaanbursa9@gmail.com>
2017-01-27 12:09:32 +0100

    pull

--- js/main.js
+++ js/main.js
@@ -3,26 +3,21 @@
 
 var navPositionY = $('.navbar').offset().top;
 
-function logScroll(){
-  
-if ($(window).scrollTop() >= navPositionY){
-  $('.navbar')
-    .css ('position','fixed')
-    .css('top','0')
-    .css ('right','0')
-    .css ('z-index', '2')
-    .css('width', '100%')
-    .css('background-color', 'black')
-    .css('opacity', '0.5')
-}
-else if ($(window).scrollTop() == 0){
-  $('.navbar')
-  .css('background-color', 'transparent')
-  .css ('border-style', 'none')
-} else {
-  $('.navbar')
-   .css ('position','relative')   
-  }
-}
+  function logScroll() {
 
-window.onscroll = logScroll;
+   
+    if ($(window).scrollTop() >= navPositionY) {
+      $('.navbar')
+        .css('position', 'fixed')
+        .css('top', '0')
+        .css('left', '0')
+        .css('width', '100%')
+    } else {
+      $('.navbar')
+        .css('position', 'fixed')
+        
+
+    }
+  }
+
+  window.onscroll = logScroll;
